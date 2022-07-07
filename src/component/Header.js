3ff0113349978df762f4f1b614f3ba446f70d149
SH-Won <1317good@naver.com>
2022-07-07 17:30:32 +0900

    style

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,9 +4,12 @@ const Header = () => {
     const items = ['INTRO','SKILL','PROJECT'];
   return (
     <nav className="nav">
+        <h1 className="nav__title">sh</h1>
+        <ul className="nav__item-list">
         {items.map(item => (
             <li className="nav__item"><a href={'#'+item}>{item}</a></li>
         ))}
+        </ul>
     </nav>
   )
 }
